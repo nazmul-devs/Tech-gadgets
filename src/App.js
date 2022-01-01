f45f08e27a873b6ef7861727ef45b80c7b9571d1
Nazmul Hosen <nazmulhosenm668@gmail.com>
2022-01-01 17:07:20 +0600

    Create header component and install react-router and react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./Pages/Header/Header";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<Header />
+				<h2>
+					Hello Lorem ipsum dolor sit amet consectetur adipisicing elit.
+					Repellendus animi aliquid facere dolorem explicabo cumque et
+					alias aspernatur sint quas?
+				</h2>
+			</header>
+		</div>
+	);
 }
 
 export default App;
