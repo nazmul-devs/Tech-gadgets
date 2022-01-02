8a9e856c41a396198b5248ba44c2c9066f3f5678
taseenbappi <taseen.bappi@gmail.com>
2022-01-02 14:45:31 +0600

    #1_cat_ticket demo catagories component added and bootstrap js and css link added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./Pages/Header/Header";
+import Catagories from "./Pages/Home/Catagories/Catagories";
 
 function App() {
 	return (
 		<div className="App">
-			<header className="App-header">
-				<Header />
-				<h2>
-					Hello Lorem ipsum dolor sit amet consectetur adipisicing elit.
-					Repellendus animi aliquid facere dolorem explicabo cumque et
-					alias aspernatur sint quas?
-				</h2>
-			</header>
+
+			<Header />
+			<h2>
+				Hello Lorem ipsum dolor sit amet consectetur adipisicing elit.
+				Repellendus animi aliquid facere dolorem explicabo cumque et
+				alias aspernatur sint quas?
+			</h2>
+			<Catagories></Catagories>
+
 		</div>
 	);
 }
