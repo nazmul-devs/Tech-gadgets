ba6be6fb02ddac9ffb55c50fa984ef1bc1252508
Nazmul Hosen <nazmulhosenm668@gmail.com>
2022-01-03 11:16:19 +0600

    some change

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,7 +1,5 @@
 import React from "react";
 import "./banner.css";
-const bannerImg =
-	"https://images.unsplash.com/photo-1515940175183-6798529cb860?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Z2FkZ2V0fGVufDB8fDB8fA%3D%3D&w=1000&q=80";
 
 const Banner = () => {
 	return (
@@ -13,8 +11,6 @@ const Banner = () => {
 				<p className="text-justify my-3">
 					Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste
 					atque animi eaque temporibus ducimus laborum. Lorem ipsum dolor,
-					sit amet consectetur adipisicing elit. Delectus esse architecto
-					animi, accusamus illum tenetur consequuntur vel perspiciatis sint
 					id!
 				</p>
 				<button className="btn explore-btn rounded-pill px-5">
