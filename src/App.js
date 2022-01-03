24ea7a6b95979d9d2b10e323956bde0dfec19246
Nazmul Hosen <nazmulhosenm668@gmail.com>
2022-01-03 21:40:13 +0600

    nowshed vai code accepted

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ function App() {
 					<Route path="/register" element={<Register />} />
 					<Route path="*" element={<NoMatch />} />
 				</Routes>
+				<Footer />
 			</BrowserRouter>
 		</div>
 	);
