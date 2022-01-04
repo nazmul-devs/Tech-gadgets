1888d53de161f1bcde752db4a7202c5b55769607
taseenbappi <taseen.bappi@gmail.com>
2022-01-05 00:44:07 +0600

    authenticaion and context api added successfully

--- src/context/AuthProvider.js
+++ src/context/AuthProvider.js
@@ -1,5 +1,5 @@
 import React, { createContext } from 'react';
-import useFirebase from '../Hooks/UseFirebase';
+import useFirebase from '../Hooks/useFirebase';
 
 export const AuthContext = createContext();
 
